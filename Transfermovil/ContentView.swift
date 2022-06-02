5f4568d05663121dc77817a1f73826d695261e6d
Víctor O. Vento <victorovento@icloud.com>
2022-06-02 18:46:31 -0400

    Initial Commit

---
+++ Transfermovil/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Transfermovil
+//
+//  Created by Víctor on 2/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Transfermovil/TransfermovilApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransfermovilApp.swift
+//  Transfermovil
+//
+//  Created by Víctor on 2/6/22.
+//
+
+import SwiftUI
+
+@main
+struct TransfermovilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
